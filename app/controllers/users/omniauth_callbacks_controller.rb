d4bb5b54923f3861c785e1db6b14d3c5857904b0
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-07-17 17:34:05 +0530

    facebook invitation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     access_token = request.env["omniauth.auth"]
-    
-    #raise access_token.inspect
-    
     data = access_token.extra.raw_info    
     provider = access_token.provider
     
@@ -13,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       name =  data.name
       first_name =  data.first_name
       last_name =  data.last_name          
-      image_path =  access_token.info.image          
+      image_path =  access_token.info.image       
       uid =  data.id
     else                                       
       render :text => '--- provider, #{@provider_route}, not supported ---'  
@@ -26,9 +23,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if !auth.nil?
         flash[:notice] = "Signed in successfully"
         sign_in_and_redirect(:user, auth)
-      else  
-        user = User.new(:provider => provider, :uid => uid, :name => name, :email => email, :fname => first_name, :lname => last_name, :image_url => image_path , :token => access_token.credentials.token, :password => Devise.friendly_token[0,20])                        
-        #raise user.inspect
+      else         
+        user = User.new(:provider => provider, :uid => uid, :email => email, :fname => first_name, :lname => last_name, :image_url => image_path , :token => access_token.credentials.token, :password => Devise.friendly_token[0,20])        
         if user.save!
           flash[:notice] = "New user signed in successfully." 
           sign_in_and_redirect(:user, user)          
@@ -37,20 +33,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         end
       end
     end
-    
-    
-    
-    
-    #@user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    #if @user.persisted?
-    #  flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-    #  sign_in_and_redirect @user, :event => :authentication
-    #else
-    #  session["devise.facebook_data"] = request.env["omniauth.auth"]
-    #  redirect_to new_user_registration_url
-    #end
   end
+  
   def passthru
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
     # Or alternatively,
