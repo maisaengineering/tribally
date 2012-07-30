76dcdb836350f3b1ddf9416986928d2cb530c08d
maisapride2 <maisapride@maisapride.(none)>
2012-07-30 10:00:43 +0530

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,17 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all    
+    @products = Product.all
   end
 
   def show
     @product = Product.find(params[:id])
+    @comment = Comment.find(params[:id])
   end
 
   def new
     @product = Product.new
-    @product.offers.build 
+    @product.offers.build
 
   end
 
@@ -19,8 +20,8 @@ class ProductsController < ApplicationController
     as = @product.offers
   end
 
-  def create    
-    @tribe = Tribe.where(:_id => params[:tribe]).first    
+  def create
+    @tribe = Tribe.where(:_id => params[:tribe]).first
     @tribe.products.create(:product_name => params[:product_name], :members => [Member.new(:uid => current_user.uid)])
     redirect_to tribe_products_tribes_path(:id => @tribe._id)
   end
@@ -29,22 +30,24 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @product.offers.destroy
       if @product.update_attributes(params[:product])
-        redirect_to @product, notice: 'Product was successfully updated.' 
+        redirect_to @product, notice: 'Product was successfully updated.'
       else
-        render action: "edit" 
-      end 
+        render action: "edit"
+      end
   end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to products_url 
+    redirect_to products_url
     end
 
   def add_comment
-	product = Product.where(:_id => params[:product_id]).first	
-	product.comments.push([Comment.new(:user_id => params[:user_id], :body => params[:body])])
-	redirect_to products_url
-   end
+product = Product.where(:_id => params[:product_id]).first
+product.comments.push([Comment.new(:user_id => params[:user_id], :body => params[:body])])
+redirect_to products_url
+  end
 end
+
+
 
