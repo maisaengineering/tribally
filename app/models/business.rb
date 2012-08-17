2566cb94dd7e6689dfa9ceb0b3593b14472fdb36
maisaengineering <contact@maisasolutions.com>
2012-08-17 11:47:11 +0530

    changes in business

--- app/models/business.rb
+++ app/models/business.rb
@@ -5,16 +5,17 @@ class Business
   
   field :business_name, :type => String
   field :website, :type => String
-  field :offers_history, :type => String
-  field :invoices, :type =>String
-  field :account_data, :type =>String
+  field :business_email, :type => String
+  #field :offers_history, :type => String
+  #field :invoices, :type =>String
+  #field :account_data, :type =>String
 
-  #validates_presence_of :business_name
+  validates_presence_of :business_name
   validates_presence_of :website
   #validates_presence_of :offers_history
   #validates_presence_of :invoices
   #validates_presence_of :account_data
   
-  #attr_accessible :business_name, :website, :offers_history, :invoices, :account_data
+  attr_accessible :business_name, :website, :business_email, :offers_history, :invoices, :account_data
 
 end
